e41f30d8c28e7f6dd0e31001c73a23ba21aba578
Vincent Cannataro <vincent.cannataro@yale.edu>
2018-07-23 15:50:48 -0400

    before revisions

--- analysis_of_TCGA_HPVstatus.R
+++ analysis_of_TCGA_HPVstatus.R
@@ -25,14 +25,83 @@ HNSC.selection.output$Unique_patient_identifier[which(HNSC.selection.output$Uniq
 
 BROAD.clinical <- read.csv("input_data/BROAD/gdac.broadinstitute.org_HNSC.Merge_Clinical.Level_1.2016012800.0.0/HNSC.clin.merged.txt",header = F,stringsAsFactors = F,sep="\t",row.names = 1)
 
+
+table(as.character(BROAD.clinical["patient.anatomic_neoplasm_subdivision",]))
+
 head(BROAD.clinical)
 BROAD_patients <- toupper(BROAD.clinical["patient.bcr_patient_barcode",])
 
 toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_status",])
 
-BROAD_HPV_status <- matrix(data = c(toupper(BROAD.clinical["patient.bcr_patient_barcode",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_status",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_call_1",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_call_2",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_call_3",])),nrow = length(toupper(BROAD.clinical["patient.bcr_patient_barcode",])),ncol = 5,byrow = F)
+BROAD_HPV_status <- matrix(data = c(toupper(BROAD.clinical["patient.bcr_patient_barcode",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_status",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_call_1",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_call_2",]),toupper(BROAD.clinical["patient.hpv_test_results.hpv_test_result.hpv_call_3",]),toupper(BROAD.clinical["patient.hpv_status_by_ish_testing",]),toupper(BROAD.clinical["patient.hpv_status_by_p16_testing",])),nrow = length(toupper(BROAD.clinical["patient.bcr_patient_barcode",])),ncol = 7,byrow = F)
 
 BROAD_HPV_status <- BROAD_HPV_status[BROAD_HPV_status[,1] %in% unique(HNSC.MAF$Unique_patient_identifier),] # just the tumors with sequencing data
+
+
+colnames(BROAD_HPV_status) <- c("patient","PCR_Result","PCR_result1","PCR_result2","PCR_result3","by_ISH","by_p16")
+
+
+# adding data from virusscan
+
+BROAD_HPV_status <- cbind(BROAD_HPV_status,NA)
+colnames(BROAD_HPV_status) <- c(colnames(BROAD_HPV_status)[1:7],"by_VirusScan")
+
+
+for(i in 1:nrow(BROAD_HPV_status)){
+ if(BROAD_HPV_status[i,"patient"] %in% HNSC.MAF.hpvpos$Unique_patient_identifier){
+   BROAD_HPV_status[i,"by_VirusScan"] <- "POSITIVE"
+ }
+  if(BROAD_HPV_status[i,"patient"] %in% HNSC.MAF.hpvneg$Unique_patient_identifier){
+    BROAD_HPV_status[i,"by_VirusScan"] <- "NEGATIVE"
+  }
+}
+
+head(BROAD_HPV_status)
+
+
+library(tidyverse)
+
+HPV_status_counts <- as_tibble(BROAD_HPV_status) %>%
+  group_by(by_ISH,by_p16,by_VirusScan) %>%
+  summarize(Counts=n())
+
+
+length(which(!is.na(BROAD_HPV_status[,"by_ISH"])))
+
+length(which(!is.na(BROAD_HPV_status[,"by_p16"])))
+
+
+rownames(BROAD_HPV_status) <- BROAD_HPV_status[,"patient"]
+
+BROAD_HPV_status[c("TCGA-BA-A6DF",
+                 "TCGA-CN-A63Y",
+                 "TCGA-CN-A640",
+                 "TCGA-CQ-7064",
+                 "TCGA-IQ-A61K",
+                 "TCGA-IQ-A61L",
+                 "TCGA-MT-A67G","TCGA-H7-A6C5"),c("by_ISH","by_p16","by_VirusScan")]
+
+
+all.3.available <- NULL
+
+for(i in 1:nrow(BROAD_HPV_status)){
+ if(all(!is.na(BROAD_HPV_status[i,6:8]))){
+  all.3.available <- rbind(all.3.available,BROAD_HPV_status[i,c(1,6:8)]) 
+ }
+}
+
+all.3.available <- cbind(all.3.available,NA)
+colnames(all.3.available)[5] <- "All equal"
+for(i in 1:nrow(all.3.available)){
+ if(all.3.available[i,2] ==  all.3.available[i,3] & all.3.available[i,3] == all.3.available[i,4]){
+   all.3.available[i,5] <- T
+ }else{
+   all.3.available[i,5] <- F
+ }
+}
+
+
+all.3.available
 
 head(BROAD_HPV_status)
 nrow(BROAD_HPV_status)
@@ -43,7 +112,7 @@ rownames(BROAD_HPV_status) <- BROAD_HPV_status[,1]
 
 BROAD_HPV_status[TCGA_positives,]
 
-BROAD_HPV_status
+View(BROAD_HPV_status)
 
 nrow(BROAD_HPV_status[which(!is.na(BROAD_HPV_status[,3])),])
 BROAD_HPV_status[which(BROAD_HPV_status[,2]=="NEGATIVE"),]
